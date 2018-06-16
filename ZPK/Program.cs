a7e412e18f17824fb866acc0e8898f365895a0f9
AJ Zane <androiddreams@ajzane.com>
2018-06-16 11:14:47 -0700

    Setup: Adds Hello World .netcore cli project

---
+++ ZPK/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ZPK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
