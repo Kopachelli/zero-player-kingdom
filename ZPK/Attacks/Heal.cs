727a7ca38dd465b4e3e90e073272353770238e3a
AJ Zane <androiddreams@ajzane.com>
2018-07-07 13:10:16 -0700

    My Bottle Is Empty - Makes healing attacks extend the base class better

--- ZPK/Attacks/Heal.cs
+++ ZPK/Attacks/Heal.cs
@@ -8,19 +8,19 @@
         /// <param name="healAmount">How much damage this heals</param>
         public Heal(int healAmount)
         {
-            damage = healAmount;
+            damage = healAmount * -1;
         }
         
         /// <summary>
         /// Executes the attack against a target
         /// </summary>
         /// <param name="attacker">The entity attacking</param>
-        /// <param name="target">The target being attacked</param>
+        /// <param name="target">Unused - The target being attacked</param>
         /// <returns>The amount of damage dealt</returns>
         public override int Execute(Person attacker, Person target)
         {
-            attacker.HP += damage;
-            return damage * -1;
+            // This attack applies a negative amount of damage to the executor
+            return base.Execute(attacker, attacker);
         }
     }
 }
